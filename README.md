d3f86e45123feb5bde4b2c41c738fe7a3418bc1e
Rajan Shrestha <rajan.shr09@gmail.com>
2023-10-14 13:00:09 -0600

    Setup conf/conf.js  appwrite/auth.js file

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 Step 1: Configure .env file and appwrite
+Step 2: Configure conf folder and then appwrite folder
+Step 3: import file from conf folder in appwrite
+Step 4: configure appwrite auth.js file for signup, login, logout
+
 
---
+++ src/appwrite/auth.js
@@ -0,0 +1,49 @@
+import { Client, Account, ID } from "appwrite";
+import conf from "../conf/conf";
+
+export class authService {
+    client = new Client();
+    account;
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId)
+        this.account = new Account(this.client)
+    }
+
+    // For Sign Up Account
+    async createAccount({ name, gmail, username, password }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), name, gmail, username, password);
+            if (userAccount) {
+                console.log('User created successfully ', userAccount);
+                return this.login({ username, password })
+            } else {
+                return userAccount
+            }
+        } catch (error) {
+            console.log('Error ', error)
+        }
+    }
+
+    // For Login Account
+    async login({ username, password }) {
+        try {
+            await this.account.createEmailSession(username, password)
+        } catch (error) {
+            console.log('Error ', error)
+        }
+    }
+
+    // For Logout
+    async logout() {
+        try {
+            this.account.deleteSessions()
+        } catch (error) {
+            console.log('Error ', error)
+        }
+    }
+}
+
+const authService = new authService();
+export default authService;
