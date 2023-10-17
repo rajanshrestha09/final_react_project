68426d9d6123f6286589ff7afc948b2bcd769b69
Rajan Shrestha <rajan.shr09@gmail.com>
2023-10-16 19:48:37 -0600

    Add more feature to the store

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -3,13 +3,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState={
-    
+    status: false,
+    userData: null
 }
 
 const authSlice = createSlice({
     name: 'auth', //Can give any name
     initialState,
     reducers:{
-        
+        login: (state, action) =>{
+            state.status = true,
+            state.userData = action.payload.payload
+        },
+
+        logout: (state, action) =>{
+            state.status = false,
+            state.userData = null
+        }
     }
 })
+
+export const {login, logout} = authSlice.actions;
+export default authSlice.reducer;
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authService from "../appwrite/auth";
 
 export const store = configureStore({
     reducer:{
-
+        auth : authService,
+        // Add more function
     },
 })
