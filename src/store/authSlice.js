a0724998243c0793204d1aaa37900489817d4678
Rajan Shrestha <rajan.shr09@gmail.com>
2023-10-16 18:35:40 -0600

    Basic configure for react-redux toolkit

---
+++ src/store/authSlice.js
@@ -0,0 +1,15 @@
+
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState={
+    
+}
+
+const authSlice = createSlice({
+    name: 'auth', //Can give any name
+    initialState,
+    reducers:{
+        
+    }
+})
