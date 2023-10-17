2a752a506c4f6224dc0a930aece482fc347b0f7e
Rajan Shrestha <rajan.shr09@gmail.com>
2023-10-16 21:40:29 -0600

    Add React-router-dom and configure all

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,36 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store/store.js";
+import { BrowserRouter, RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+    <RouterProvider router={router}/>
     </Provider>
   </React.StrictMode>
 );
