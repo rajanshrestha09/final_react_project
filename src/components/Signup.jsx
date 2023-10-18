6a1c42033172d50d76a7bbd085e8ae5f25ff174c
Rajan Shrestha <rajan.shr09@gmail.com>
2023-10-18 10:52:18 -0600

    Use of ...register=> react-hook-form and (useDispatch and useSelector)=>react-redux

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,17 +1,56 @@
-import React from 'react'
-import {Input, Button} from './index'
-import {useForm} from 'react-hook-form'
+import React from "react";
+import { Input, Button } from "./index";
+import { useForm } from "react-hook-form";
+import authService from "../appwrite/auth";
+import { useSelector, useDispatch } from "react-redux";
 
 function Signup() {
-  const {register, handleSubmit} = useForm();
+  const { register, handleSubmit } = useForm();
+  const dispatch = useDispatch();
+  // useSelector();
 
-  const create = () =>console.log('button signup clicked')
+  const create = (data) => console.log(data);
   return (
     <form onSubmit={handleSubmit(create)}>
-      <Input />
+      <Input
+        label="Name"
+        type="text"
+        placeholder="Enter your full name"
+        className="m-2 border-2"
+        {...register("name", {
+          required: true,
+        })}
+      />
+      <Input
+        label="Email"
+        type="email"
+        placeholder="Enter your email"
+        className="m-2 border-2"
+        {...register("email", {
+          required: true,
+        })}
+      />
+      <Input
+        label="Username"
+        type="text"
+        placeholder="Enter your username"
+        className="m-2 border-2"
+        {...register("username", {
+          required: true,
+        })}
+      />
+      <Input
+        label="Password"
+        type="password"
+        placeholder="Enter your password"
+        className="m-2 border-2"
+        {...register("passwrod", {
+          required: true,
+        })}
+      />
       <Button>Signup</Button>
     </form>
-  )
+  );
 }
 
-export default Signup
+export default Signup;
