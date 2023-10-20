01c3a4dbf99abffa7df59c85c0f28191a33aa9f5
Rajan Shrestha <rajan.shr09@gmail.com>
2023-10-20 10:01:17 -0600

    Basic setup for config.js => Database

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -1,1 +1,58 @@
 import { Client, Databases } from "appwrite";
+import conf from "../conf/conf";
+
+export class Service {
+    client = new Client();
+    databases;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId)
+        this.databases = new Databases(this.client)
+    }
+
+    async createPost() {
+        try {
+
+        } catch (error) {
+
+        }
+    }
+
+    async updatePost() {
+        try {
+
+        } catch (error) {
+
+        }
+    }
+
+    async deletePost() {
+        try {
+
+        } catch (error) {
+
+        }
+    }
+
+    async getPost() {
+        try {
+
+        } catch (error) {
+
+        }
+    }
+
+    async getPosts() {
+        try {
+
+        } catch (error) {
+
+        }
+    }
+
+}
+
+const service = new Service();
+export default service;
